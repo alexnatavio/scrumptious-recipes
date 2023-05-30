62935d5018f87d3fbcbfac873b89a7e867b74139
Alex <alex.natavio@gmail.com>
2023-05-30 14:48:19 -0400

    update files to includ redirect to homepage

---
+++ recipes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from django.contrib import admin
+from recipes.views import show_recipe, recipe_list, create_recipe, edit_recipe
+
+urlpatterns = [
+    path('recipes/', recipe_list, name="recipe_list"),
+    path('recipes/<int:id>', show_recipe, name="show_recipe"),
+    path('recipes/create', create_recipe, name="create_recipe"),
+    path('recipes/<int:id>/edit/', edit_recipe, name="edit_recipe"),
+
+
+]
